544cf031506766009e6215bc08f8109e4e1e11fc
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-07 19:47:52 +0300

    sad

--- 0x00-ySQL_Advanced/0-uniq_users.sql
+++
@@ -1,9 +0,0 @@
--- this is a script that creates a table
--- with a unique constraint
-
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-)
-;
