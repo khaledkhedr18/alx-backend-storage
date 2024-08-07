0a2548eb97f752d8d6ce6d990f22589708d0267f
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-07 19:48:25 +0300

    Another Completed Task

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- this is a script that creates a table
+-- with a unique constraint
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+)
+;
---
+++ 0x00-MySQL_Advanced/git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git add *
+git commit -m "Another Completed Task"
+git push
+
+echo "All files Pushed Successfully"
