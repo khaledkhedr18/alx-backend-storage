9e59dcc6f92ca7ac370881a4da70da089fc4f458
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-07 19:49:27 +0300

    Another Completed Task

--- trashtest/0-uniq_users.sql
+++ trashtest/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- this is a script that creates a table
 -- with a unique constraint
 
-CREATE TABLE users (
+CREATE TABLE users IF NOT EXISTS (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
