ab3609b82f5794fbd6f52f1791994167890bb2d8
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-09 01:17:48 +0300

    all the remaining tasks

--- trashtest/0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- this is a script that creates a table with unique users
--- id, email, name
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
