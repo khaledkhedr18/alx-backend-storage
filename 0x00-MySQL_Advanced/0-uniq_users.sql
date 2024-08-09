1694264999fc098a66ed3d262d302345c6e27ac8
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-09 21:47:17 +0300

    updated solutions for the project

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
--- this is a script that creates a table
--- with a unique constraint
-
-CREATE TABLE IF NOT EXISTS users (
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-)
+);
