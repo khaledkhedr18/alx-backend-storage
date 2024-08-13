0d1a0be1505c5bb1fbed7591ed538868ded62a5c
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-14 02:45:58 +0300

    task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-import pymongo
 '''
 a Python Function that inserts a ndocument in a collection based
 on kwargs
 '''
 
 
-def insert_school(mongo_collection, **kwargs):
+def insert_school(mango_collection, **kwargs):
     '''inserts a new document in a collection based on kwargs
     '''
-    for insertion in kwargs:
-        mongo_collection.insert(insertion)
+    result = mango_collection.insert_one(kwargs)
+    return result.inserted_id
