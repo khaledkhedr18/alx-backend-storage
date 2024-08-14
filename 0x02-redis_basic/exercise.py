d99d4c834664b78fe576a9113148246e32096c53
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-15 01:01:07 +0300

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -30,3 +30,39 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable = None):
+        """
+        Retrieves data from the Redis database.
+        Parameters:
+            key (str): The key to retrieve data from.
+            fn (callable): An optional callable to convert the data to the desired type.
+        Returns:
+            The retrieved data.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is None:
+            return value
+        return fn(value)
+
+    def get_str(self, key: str):
+        """
+        Retrieves a string from the Redis database.
+        Parameters:
+            key (str): The key to retrieve data from.
+        Returns:
+            str: The retrieved string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str):
+        """
+        Retrieves an integer from the Redis database.
+        Parameters:
+            key (str): The key to retrieve data from.
+        Returns:
+            int: The retrieved integer.
+        """
+        return self.get(key, fn=int)
